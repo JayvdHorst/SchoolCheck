b9c34e7d973fb2d908dc6c71c053ae3db942354a
LMesker <luukbonaire@gmail.com>
2016-12-01 11:16:13 +0100

    updated index

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
 <?php
 
+//define host
+include 'includes/config.php';
+//make connection
+include 'includes/database.php';
+
 include 'views/head.html';
 
 include 'views/header.html';
 
+$action=isset($_GET['action'])?$_GET['action']:'home';
 
+    switch($action) {
+        case 'admin':
+
+            include 'views/insert_form.html';
+            include 'model/cms.php';
+            break;
+
+        case 'home':
+			
+            echo 'home';
+            break;
+    }
+	
 include 'views/footer.html';
+
+?>
